d47bc60ac44cda7de06f595b50825b4ed1e6d042
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 03:22:31 +0000

    Kontanta pada php

---
+++ 06/code/konstanta.php
@@ -0,0 +1,12 @@
+<?php // file: belajar-konstanta.php
+
+// membuat konstanta
+define('VERSION', '1.0.0');
+
+const SITE_NAME  = "Kibby Kazan";
+const BASE_URL    = "https://github.com/Kibbykazan03";
+
+// cetak nilai konstanta
+echo "Site name: " . SITE_NAME . "<br/>";
+echo "URL: " . BASE_URL . "<br/>";
+echo "Version: " . VERSION . "<br/>";
