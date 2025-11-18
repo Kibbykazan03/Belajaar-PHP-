da51455ac764c3c183b135944a163cd1171a3224
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 03:20:26 +0000

    operator aritmatika

---
+++ 07/code/operator-arimatika.php
@@ -0,0 +1,38 @@
+<?php
+
+$a = 16;
+$b = 3;
+
+// penjumlahan
+$c = $a + $b;
+echo "$a + $b = $c";
+echo "<hr>";
+
+
+// pengurangan
+$c = $a - $b;
+echo "$a - $b = $c";
+echo "<hr>";
+
+// Perkalian
+$c = $a * $b;
+echo "$a * $b = $c";
+echo "<hr>";
+
+// Pembagian
+$c = $a / $b;
+echo "$a / $b = $c";
+echo "<hr>";
+
+// Sisa bagi
+$c = $a % $b;
+echo "$a % $b = $c";
+echo "<hr>";
+
+// Pangkat
+$c = $a ** $b;
+echo "$a ** $b = $c";
+echo "<hr>";
+
+
+?>
