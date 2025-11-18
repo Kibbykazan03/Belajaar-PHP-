91e49e6dc0c08ba6f9c8e4cacb37a6be7696b9a4
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 03:29:53 +0000

    penulisan statement

---
+++ 03/code/penulisan-statement.php
@@ -0,0 +1,6 @@
+<?php
+echo "ini statement 1";
+echo "ini statement 2";
+$a = $b + $c;
+
+// Statement dan ekspresi adalah instruksi yang akan diberikan ke komputer dan harus diakhiri dengan titik koma (;)
