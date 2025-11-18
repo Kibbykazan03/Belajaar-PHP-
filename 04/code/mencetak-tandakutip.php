c463b2924ec4bbd85f9e00c27fe6a22f1927b6f1
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 03:28:54 +0000

    mencetak tanda kutip di php

---
+++ 04/code/mencetak-tandakutip.php
@@ -0,0 +1,10 @@
+<?php
+
+// echo 'hari ini hari jum'at';
+// maka hasilnya akan eror
+
+echo 'hari ini hari jum\'at';
+// caranya kita ingin mencetak tanda kutip dengan menggunakan tanda back slash
+
+
+?>
