0e1514eca946f2b5d9f5844eb42ac4dec3821dc5
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 02:55:59 +0000

    membuat array

---
+++ 10/code/membuat_array.php
@@ -0,0 +1,14 @@
+<?php
+
+// membuat array kosong
+$buah = array();
+$hobi = [];
+
+// membuat array sekaligus mengisinya
+$minuman = array("Kopi", "Susu", "jus jeruk");
+$makanan = ["Mie Ayam", "Es krim", "coklat"];
+
+// membuat array dengan mengisi indeks tertentu
+$anggota[1] = "Kibby";
+$anggota[2] = "Khattab";
+$anggota[0] = "Kenarok";
