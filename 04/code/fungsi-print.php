202c9dd5891307a327faae41319049869fee26f2
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 03:29:33 +0000

    fungsi print pada php

---
+++ 04/code/fungsi-print.php
@@ -0,0 +1,11 @@
+<?php
+
+print "<h2>Belajar PHP dari Nol!</h2>";
+
+print "Hello world!
+<br>";
+
+print "Belajar mencetak teks di PHP!";
+
+// Fungsi print mirip dengan echo, namun print hanya dapat mencetak satu argumen saja sedangkan echo dapat mencetak lebih dari satu argumen.
+?>
