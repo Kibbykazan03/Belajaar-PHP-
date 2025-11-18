dac1b6234ca9238c38cc986980f2b350e03eb856
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 03:30:03 +0000

    penulisan case

---
+++ 03/code/penulisan-case.php
@@ -0,0 +1,18 @@
+<?php
+
+$nama = "KIBBY KAWENA KAZAN";
+$NAMA = "Abeyy";
+
+// dari kedua variabel tersebut todak lah sama, penulisan huruf harus sangat diperhatikan karena bisa menyebabkan error.
+
+// disarankan untuk konsisten dalam penulisan nama variabel dengan huruf kecil dan konstanta dengan huruf besar.
+
+
+const INI_KONSTANTA = 123;
+$ini_variabel = 23;
+$iniJugaVariabel = 49;
+
+// $iniJugaVariabel ini disebut dengan camelCase, yang dimana variabel terdiri dari dua suku kata maka memsisahkannya dengan huruf kapital
+
+?>
+
