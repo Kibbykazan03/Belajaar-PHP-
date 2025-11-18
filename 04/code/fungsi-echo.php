2094fc7fa190a986a040f4af7675a383e4e2158f
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 03:29:19 +0000

    fungsi echo

---
+++ 04/code/fungsi-echo.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h2>Belajar PHP itu mudah!</h2>";
+
+echo("Hallooo semuanya!
+
+<br>");
+echo "Aku sedang belajar PHP nihhh
+!<br>";
+
+echo "Ini ", "teks ", "yang ", "dibuat ", "terpisah.";
+
+?>
