049d27ea762f097c10b31268cb8d96fea06c7e75
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 03:23:54 +0000

    membuat variabel php

---
+++ 05/code/membuat-varibael.php
@@ -0,0 +1,18 @@
+<?php 
+
+$nama_barang = "Kopi C++";
+$harga = 4000;
+$stok = 40;
+
+// tanda samadengan (=) digunakan untuk mengisi nilai ke variabel
+
+// *kita juga bisa mengisi ulang nilai yang baru
+        
+
+        $stok = 40;
+// membuat variabel baru
+
+        $stok = 34;
+// mengisi ulang variabel dengan nilai baru
+
+?>
