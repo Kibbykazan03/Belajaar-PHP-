f64af1231aeb018c917ef6811a1d5dcda37a56f8
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 02:54:50 +0000

    menampilkan array

---
+++ 10/code/menampilkan_array.php
@@ -0,0 +1,8 @@
+<?php
+// membuat array
+$barang = ["mie ayam", "eskrim", "coklat"];
+
+// menampilkan isi array dengan perulangan for
+for($i=0; $i < count($barang); $i++){
+    echo $barang[$i]."<br>";
+}
