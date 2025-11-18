b3c74f45cfbddaff670064356c0e902522337e4b
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 03:23:22 +0000

    mengenal tipe data

---
+++ 05/code/mengenal_tipe-data.php
@@ -0,0 +1,26 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'P';
+
+// tipe data string (teks)
+$nama_lengkap = "KIBBY KAWENA KAZAN";
+
+// tipe data integer
+$umur = 16;
+
+// tipe data float
+$berat = 54.7;
+
+// tipe data float
+$tinggi = 169;
+
+// tipe data boolean
+$Pelajar = true;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "Pelajar: $Pelajar";
