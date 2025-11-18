76429d0b0aa3bca3f070c6056781fd99fa11e6b5
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 03:22:44 +0000

    kontanta define

---
+++ 06/code/konstanta-define.php
@@ -0,0 +1,15 @@
+<?php
+// membuat konstanta dengan fungsi define()
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'petanikode');
+define('DB_PASS', 'R4Hasia');
+define('DB_NAME', 'belajar');
+
+// membuat konstanta dengan kata kunci const
+const API_KEY = "1234";
+
+const SITE_NAME = "KIBBY";
+
+echo "Nama situs: " . SITE_NAME;
+
+?>
