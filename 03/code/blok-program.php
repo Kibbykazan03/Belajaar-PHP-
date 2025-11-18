f8b0e3a1aa505439bb207ccc9fc5bb3ea511b105
KibbyKazan03 <Kibbykazan@gmail.com>
2025-11-18 03:30:27 +0000

    blog program pada php

---
+++ 03/code/blok-program.php
@@ -0,0 +1,12 @@
+<?php
+
+if ($umur > 18){
+    echo "Kamu sudah dewasa";
+    echo "Selamat datang";
+    echo "Kamu boleh minum kopi";
+}
+
+// blok program PHP dibungkus dengan kurung kurawal yang berisi kumpulan statement dan ekspresi.
+
+
+?>
